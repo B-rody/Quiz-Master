8178a8ae9f9cb121a20a569d50d5e1e005f34b5a
B-rody <brschulk@outlook.com>
2023-05-18 17:48:30 -0700

    Created canvas for the quiz itself. Added question text, answer options, scriptable question objects, UI updates upon answer selection, and started working on the question and answer timer functionality.

---
+++ Assets/Scripts/Quiz.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Quiz : MonoBehaviour
+{
+
+    [SerializeField] TextMeshProUGUI questionText;
+    [SerializeField] QuestionSO question;
+    [SerializeField] GameObject[] answerButtons;
+    int correctAnswerIndex;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
+
+    void Start()
+    {
+        GetNextQuestion();
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+        Image buttonImage = answerButtons[index].GetComponent<Image>();
+        correctAnswerIndex = question.GetCorrectAnswerIndex();
+        if (index == correctAnswerIndex)
+        {
+            questionText.text = "Correct!";
+            buttonImage.sprite = correctAnswerSprite;
+        }
+        else
+        {
+            questionText.text = "Sorry, the correct answer was: \n" + 
+                question.GetAnswer(correctAnswerIndex);
+            
+            buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+        SetButtonState(false);
+    }
+
+    void GetNextQuestion()
+    {
+        DisplayQuestion();
+        SetDefaultButtonSprites();
+        SetButtonState(true);
+    }
+
+    void DisplayQuestion()
+    {
+        questionText.text = question.GetQuestion();
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.GetAnswer(i);
+        }
+    }
+
+    void SetButtonState(bool state)
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            Button button = answerButtons[i].GetComponent<Button>();
+            button.interactable = state;
+        }
+    }
+
+    void SetDefaultButtonSprites()
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            Image buttonImage = answerButtons[i].GetComponent<Image>();
+            buttonImage.sprite = defaultAnswerSprite;
+        }
+    }
+
+}
